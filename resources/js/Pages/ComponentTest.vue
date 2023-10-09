bdb0ced22f0120898c536a30f570407ad18208f4
ko-ki <shankouhanghui15@gmail.com>
2023-10-09 14:45:23 +0900

    Second

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,17 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import InputLabel from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e);
+</script>
+
+<template>
+    <GuestLayout>
+        <InputLabel value="件名">title</InputLabel>
+        <TextInput
+        model-value="初期値"
+        @update:modelValue="emitTest"
+        ></TextInput>
+    </GuestLayout>
+</template>
